1778c713d928ca16b16bdcdf07524088e9113bf7
ipalm0423 <76423ok@gmail.com>
2015-09-19 12:39:33 +0800

    Initial Commit

---
+++ Luguo Coffee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Luguo Coffee
+//
+//  Created by 陳冠宇 on 2015/9/19.
+//  Copyright (c) 2015年 陳冠宇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Luguo CoffeeTests/Luguo_CoffeeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Luguo_CoffeeTests.swift
+//  Luguo CoffeeTests
+//
+//  Created by 陳冠宇 on 2015/9/19.
+//  Copyright (c) 2015年 陳冠宇. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Luguo_CoffeeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
